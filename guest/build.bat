c212bcc38b3efef1e2869210f41cb8d112fb309c
shrpnsld <anton.degtiar@gmail.com>
2015-09-18 14:21:14 +0300

    Removed 'vdev/' from script paths

--- guest/build.bat
+++ guest/build.bat
@@ -2,7 +2,6 @@ set VS_VERSION=%1
 set SOLUTION_PATH=%2
 set TARGET=%3
 set CONFIGURATION=%4
-set BUILD_LOG_FILE=%5
 set VCVARSALL="%ProgramFiles(x86)%\Microsoft Visual Studio %VS_VERSION%.0\VC\vcvarsall.bat"
 
 call %VCVARSALL%
