245278a449a2448db5354021ca07220749dc824b
shrpnsld <anton.degtiar@gmail.com>
2016-11-24 16:44:53 +0200

    Added 'run-vcxproj', 'detail/find_solution_for_project', 'detail/project_property', 'detail/check_paths_file'

--- detail/guest/run.bat
+++ detail/guest/run.bat
@@ -1,7 +1,9 @@
 ::
 :: Parameters
 
-set solution_dir=%1
+@echo off
+
+set project_dir=%1
 set out_dir=%2
 set target=%3
 
@@ -10,7 +12,7 @@ set target=%3
 :: Main
 
 :: Dealing with relative paths
-cd /d %solution_dir% || exit %errorlevel%
+cd /d %project_dir% || exit %errorlevel%
 cd /d %out_dir% || exit %errorlevel%
 
 %target%
