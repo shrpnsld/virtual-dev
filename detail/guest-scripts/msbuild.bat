452632f2e5be82512ec483b708cc4bc68e04e0c0
shrpnsld <anton.degtiar@gmail.com>
2017-11-30 16:21:09 +0200

    Added support for Visual Studio 2017

--- detail/guest-scripts/msbuild.bat
+++ detail/guest-scripts/msbuild.bat
@@ -10,15 +10,28 @@ set vs_version=%2
 ::
 :: Main
 
-cd /d %working_dir%
-
 setlocal enableDelayedExpansion
 
-:: getting specific VS version path and saving it in the variable
-call set vs_comntools_path=VS%vs_version%0COMNTOOLS
-set vcvarsall="!%vs_comntools_path%!\..\..\VC\vcvarsall.bat"
+:: getting specific VS version path and saving it in the variable "init_vs_dev_cmd"
+if %vs_version% lss 2017 (
+	call :prior_vs2017
+) else (
+	call :vs2017_and_later
+)
 
-call %vcvarsall% || exit 1
+call %init_vs_dev_cmd% || exit 1
 
+cd /d %working_dir%
 msbuild %VIRTUAL_DEV_ARGUMENTS%
+exit /b %ERRORLEVEL%
+
+
+:prior_vs2017
+	call set vs_comntools_path=VS%vs_version%0COMNTOOLS
+	set init_vs_dev_cmd="!%vs_comntools_path%!\..\..\VC\vcvarsall.bat"
+	exit /b 0
+
+:vs2017_and_later
+	set init_vs_dev_cmd="%ProgramFiles(x86)%\Microsoft Visual Studio\%vs_version%\Community\Common7\Tools\VsDevCmd.bat"
+	exit /b 0
 
